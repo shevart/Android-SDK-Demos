dc457144fb76e0048a68b228a0e7ee1ce3719f5c
Artem Shevchenko <shevartsoft@gmail.com>
2017-11-29 15:06:30 +0200

    work with ViewModel demo section

--- app/src/main/java/com/shevart/androidcorelearn/arch_components/ArchComponentsActivity.java
+++ app/src/main/java/com/shevart/androidcorelearn/arch_components/ArchComponentsActivity.java
@@ -1,17 +1,34 @@
 package com.shevart.androidcorelearn.arch_components;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.shevart.androidcorelearn.R;
+import com.shevart.androidcorelearn.arch_components.viewmodel.SimpleViewModelSampleActivity;
 import com.shevart.androidcorelearn.common.AbsActivity;
 
 public class ArchComponentsActivity extends AbsActivity {
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.btViewModelTopic:
+                    startActivity(new Intent(ArchComponentsActivity.this, SimpleViewModelSampleActivity.class));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Check it!");
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arch_components);
         enableToolbarBackButton();
+
+        findViewById(R.id.btViewModelTopic).setOnClickListener(clickListener);
 
     }
 }
