47bedd26483f9d7ac8c01ed44a1bf7332422e3ac
Artem Shevchenko <shevartsoft@gmail.com>
2017-11-30 16:56:10 +0200

    work with MediatorLiveData

--- app/src/main/java/com/shevart/androidcorelearn/arch_components/livedata/LiveDataDemoActivity.kt
+++ app/src/main/java/com/shevart/androidcorelearn/arch_components/livedata/LiveDataDemoActivity.kt
@@ -2,8 +2,8 @@ package com.shevart.androidcorelearn.arch_components.livedata
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.shevart.androidcorelearn.R
 import kotlinx.android.synthetic.main.activity_live_data_demo.*
 
@@ -15,5 +15,7 @@ class LiveDataDemoActivity : AppCompatActivity() {
 
         val viewModel = ViewModelProviders.of(this).get(LiveDataDemoViewModel::class.java)
         viewModel.getUserName().observe(this, Observer { tvUserName.text = it })
+
+        viewModel.getUserMessages().observe(this, Observer {tvUserMessage.text = it?.title})
     }
 }
--- app/src/main/java/com/shevart/androidcorelearn/arch_components/livedata/LiveDataDemoViewModel.kt
+++ app/src/main/java/com/shevart/androidcorelearn/arch_components/livedata/LiveDataDemoViewModel.kt
@@ -1,12 +1,17 @@
 package com.shevart.androidcorelearn.arch_components.livedata
 
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MediatorLiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.os.Handler
+import android.os.Looper
+import com.shevart.androidcorelearn.common.SimpleItem
+import com.shevart.androidcorelearn.utils.MockUtils
 
 class LiveDataDemoViewModel : ViewModel() {
     private var userNameLiveData: MutableLiveData<String>? = null
+    private var userMessagesLiveData: MediatorLiveData<SimpleItem>? = null
 
     fun getUserName() : LiveData<String> {
         if (userNameLiveData == null) {
@@ -16,7 +21,32 @@ class LiveDataDemoViewModel : ViewModel() {
         return userNameLiveData!!
     }
 
+    fun getUserMessages() : LiveData<SimpleItem> {
+        if (userMessagesLiveData == null) {
+            userMessagesLiveData = MediatorLiveData()
+            loadUserMessages()
+        }
+        return userMessagesLiveData!!
+    }
+
     private fun loadSimpleLiveData() {
         Handler().postDelayed({ userNameLiveData?.value = "Vasya" }, 1000)
+    }
+
+    private fun loadUserMessages() {
+        val handler = Handler(Looper.getMainLooper())
+        val firstDataSource = MutableLiveData<SimpleItem>()
+        val secondDataSource = MutableLiveData<SimpleItem>()
+
+        userMessagesLiveData!!.addSource(firstDataSource, {userMessagesLiveData!!.postValue(it)})
+        userMessagesLiveData!!.addSource(secondDataSource, {userMessagesLiveData!!.postValue(it)})
+
+        var timeShift = 0L
+        for (i in 1..10) {
+            timeShift += 1_000
+            handler.postDelayed({firstDataSource.value = MockUtils.SimpleItems.generateSimpleItem()}, timeShift)
+            timeShift += 1_000
+            handler.postDelayed({secondDataSource.value = MockUtils.SimpleItems.generateSimpleItem()}, timeShift)
+        }
     }
 }
