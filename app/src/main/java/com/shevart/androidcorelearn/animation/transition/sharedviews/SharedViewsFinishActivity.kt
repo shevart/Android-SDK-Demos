4c9dd92284c43b7ab9a721746c357b191e970e22
Artem Shevchenko <shevartsoft@gmail.com>
2017-12-01 14:11:35 +0200

    add two activities for demo work with shared views animation

---
+++ app/src/main/java/com/shevart/androidcorelearn/animation/transition/sharedviews/SharedViewsFinishActivity.kt
@@ -0,0 +1,13 @@
+package com.shevart.androidcorelearn.animation.transition.sharedviews
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.shevart.androidcorelearn.R
+
+class SharedViewsFinishActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shared_views_finish)
+    }
+}
