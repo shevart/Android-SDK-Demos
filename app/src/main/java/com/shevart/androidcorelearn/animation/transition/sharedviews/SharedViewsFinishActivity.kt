8c37fe26463e2a5cfb03bc5d8cef2e619e06541d
Artem Shevchenko <shevartsoft@gmail.com>
2017-12-01 16:18:36 +0200

    Work with shared views animation - add transition animation sample

--- app/src/main/java/com/shevart/androidcorelearn/animation/transition/sharedviews/SharedViewsFinishActivity.kt
+++ app/src/main/java/com/shevart/androidcorelearn/animation/transition/sharedviews/SharedViewsFinishActivity.kt
@@ -1,6 +1,7 @@
 package com.shevart.androidcorelearn.animation.transition.sharedviews
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.shevart.androidcorelearn.R
 import com.shevart.androidcorelearn.common.AbsActivity
 
@@ -10,5 +11,14 @@ class SharedViewsFinishActivity : AbsActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_views_finish)
         enableToolbarBackButton()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == android.R.id.home) {
+            supportFinishAfterTransition()
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/shevart/androidcorelearn/animation/transition/sharedviews/SharedViewsStartActivity.kt
+++ app/src/main/java/com/shevart/androidcorelearn/animation/transition/sharedviews/SharedViewsStartActivity.kt
@@ -1,8 +1,12 @@
 package com.shevart.androidcorelearn.animation.transition.sharedviews
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.ActivityOptionsCompat
+import android.view.View
 import com.shevart.androidcorelearn.R
 import com.shevart.androidcorelearn.common.AbsActivity
+import kotlinx.android.synthetic.main.activity_shared_views_start.*
 
 class SharedViewsStartActivity : AbsActivity() {
 
@@ -10,5 +14,15 @@ class SharedViewsStartActivity : AbsActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_views_start)
         enableToolbarBackButton()
+
+        btOpenDetail.setOnClickListener { openDetailScreen() }
+    }
+
+    private fun openDetailScreen() {
+        val intent = Intent(this, SharedViewsFinishActivity::class.java)
+        // Pass data object in the bundle and populate details activity.
+        val options = ActivityOptionsCompat
+                .makeSceneTransitionAnimation(this, ivSmallAvatar as View, getString(R.string.avatar_transition))
+        startActivity(intent, options.toBundle())
     }
 }
