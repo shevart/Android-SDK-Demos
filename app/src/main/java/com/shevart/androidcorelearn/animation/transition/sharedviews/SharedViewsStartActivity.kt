b633ec192048cc9a0f337d705c718e5322d9fc2b
Artem Shevchenko <shevartsoft@gmail.com>
2017-12-18 08:43:20 +0200

    work with scene transition animation

--- app/src/main/java/com/shevart/androidcorelearn/animation/transition/sharedviews/SharedViewsStartActivity.kt
+++ app/src/main/java/com/shevart/androidcorelearn/animation/transition/sharedviews/SharedViewsStartActivity.kt
@@ -3,7 +3,6 @@ package com.shevart.androidcorelearn.animation.transition.sharedviews
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.ActivityOptionsCompat
-import android.view.View
 import com.shevart.androidcorelearn.R
 import com.shevart.androidcorelearn.common.AbsActivity
 import kotlinx.android.synthetic.main.activity_shared_views_start.*
